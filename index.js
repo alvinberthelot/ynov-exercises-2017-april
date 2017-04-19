71419065e50948597e316b131d8ff712dc9bdb78
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-04-19 12:09:23 +0200

    Exercice RxJS 1.2 par Antoine

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 let chalk = require('chalk');
+let Rx = require('rxjs');
 
-var data = [3, 5, 6, 'cinq', 67, 'toto', 9];
+var dataObservable = Rx.Observable.create(function(observer){
+  observer.next(3);
+  observer.next(5);
+  observer.next(6);
+  observer.next('cinq');
+  observer.next('67');
+  observer.next('toto');
+  observer.next(9);
+  observer.complete();
+});
 
-function sum(data) {
-  return data
+console.log('OBSERVATEUR 1\n');
+dataObservable
+  .subscribe(value => {
+    console.log('Observateur 1 : ' + value);
+  });
+console.log('\n');
+
+console.log('OBSERVATEUR 2\n');
+dataObservable
   .filter(val => typeof val == 'number')
-  .reduce((acc, item) => acc + item, 0);
-}
-
-module.exports = {
-  sum: sum
-}
+  .reduce((acc, item) => acc + item, 0)
+  .subscribe(value => {
+    console.log('Observateur 2 : ' + value);
+  })
+console.log('\n');
