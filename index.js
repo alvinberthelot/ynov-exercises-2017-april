093df1106d8a970471f9e43b9495305c036072cb
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-04-19 10:28:21 +0200

    Exercice RxJS 1.1 par Mathilde

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 let chalk = require('chalk');
 
-console.log(chalk.green('Hello world'));
+var data = [3, 5, 6, 'cinq', 67, 'toto', 9];
+
+function sum(data) {
+  return data
+  .filter(val => typeof val == 'number')
+  .reduce((acc, item) => acc + item, 0);
+}
+
+console.log(chalk.green(sum(data)));
