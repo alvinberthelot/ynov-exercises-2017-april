dcbb875e8a9c486805d69ac4b8ace2d1c6c027c0
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-04-20 09:20:10 +0200

    Exercise 2.1 RxJS par Guillaume

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-let chalk = require('chalk');
-let Rx = require('rxjs');
+ let chalk = require('chalk');
 
-// var dataObservable = Rx.Observable.create(function(observer) {
-//   observer.next(3);
-//   observer.next(5);
-//   observer.next(6);
-//   observer.next('cinq');
-//   observer.next('67');
-//   observer.next('toto');
-//   observer.next(9);
-//   observer.complete();
-// });
+ var data = [3, 5, 6, 'cinq', 67, 'toto', 9];
 
-var dataObservable = Rx.Observable
-  .from([3, 5, 6, 'cinq', '67', 'toto', 9])
-  .zip(Rx.Observable.interval(1000), (value, index) => value);
-
-console.log('OBSERVATEUR 1\n');
-dataObservable
-  .subscribe(value => {
-    console.log('Observateur 1 : ' + value);
-  });
-console.log('\n');
-
-console.log('OBSERVATEUR 2\n');
-dataObservable
+function sum(data) {
+  return data
   .filter(val => typeof val == 'number')
-  .reduce((acc, item) => {
-    if (typeof item !== 'number') {
-      throw(new Error('ceci n\'est pas un nombre !!!'));
-    }
-    return acc + item
-  }, 0)
-  .subscribe(
-    value => { console.log('Observateur 2 : ' + value)},
-    error => {console.log(error)},
-    () => { console.log('Cette donnée a été calculée à partir de 4 chiffres')})
-console.log('\n');
+  .reduce((acc, item) => acc + item, 0);
+ }
+
+var promise = new Promise(function (fulfill, reject) {
+  setTimeout(fulfill, 6000, sum(data))
+})
+
+promise.then(console.log);
