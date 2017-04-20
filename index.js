3b18f72e85d4f48d014cd7b5a9f99c108beb4cec
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-04-20 10:19:19 +0200

    Exercise 2.1 RxJS avec reject par Florian

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
  let chalk = require('chalk');
+ var _ = require('lodash');
 
  var data = [3, 5, 6, 'cinq', 67, 'toto', 9];
 
 function sum(data) {
   return data
-  .filter(val => typeof val == 'number')
+  //.filter(val => typeof val == 'number')
   .reduce((acc, item) => acc + item, 0);
  }
 
 var promise = new Promise(function (fulfill, reject) {
-  setTimeout(fulfill, 6000, data)
+  if(_.every(data, val => typeof val == 'number')){
+    setTimeout(fulfill, 3000, data)
+  }
+  else {
+    setTimeout(reject, 1000, 'chiffres KO')
+  }
+
 })
 
-promise.then((data) => { console.log(sum(data)) });
+
+promise.then(
+  (data) => {
+    console.log(sum(data));
+  },
+  (msgError) => {
+    console.log(msgError);
+  }
+);
+
+
