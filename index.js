aefbd31855542e45bee3e0fb55f6bf0d136beee2
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-04-20 14:02:28 +0200

    Exercise 2.2 RxJS terminé par Kevin

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
- let chalk = require('chalk');
- var _ = require('lodash');
-
- var data = [3, 5, 6, 'cinq', 67, 'toto', 9];
-
-function sum(data) {
-  return data
-  //.filter(val => typeof val == 'number')
-  .reduce((acc, item) => acc + item, 0);
- }
-
-var promise = new Promise(function (fulfill, reject) {
-  if(_.every(data, val => typeof val == 'number')){
-    setTimeout(fulfill, 3000, data)
-  }
-  else {
-    setTimeout(reject, 1000, 'chiffres KO')
-  }
-
-})
+let chalk = require('chalk');
+let Rx = require('rxjs');
 
 
-promise.then(
-  (data) => {
-    console.log(sum(data));
-  },
-  (msgError) => {
-    console.log(msgError);
-  }
-);
+var data = [3, 5, 6, 'cinq', 67, 'toto', 9];
+
+
+var dataObservable = Rx.Observable.create(function(observer) {
+  var tmp = -1;
+  var inter = setInterval(()=> {
+    if(++tmp < data.length){
+      observer.next(data[tmp]);
+    }
+  } , 500);
+  setTimeout(()=> {
+    observer.complete();
+    clearInterval(inter)
+  } , 4000);
+});
+
+
+console.log('OBSERVATEUR 1\n');
+
+dataObservable
+  .subscribe(value => {
+    console.log('Observateur 1 : ' + value);
+  });
+
+console.log('\n');
+
+
+console.log('OBSERVATEUR 2\n');
+
+dataObservable
+  .filter(val => typeof val == 'number')
+  .reduce((acc, item) => {
+    if (typeof item !== 'number') {
+      throw(new Error('ceci n\'est pas un nombre !!!'));
+    }
+    return acc + item
+  }, 0)
+  .subscribe(
+    value => {
+
+      console.log('Observateur 2 : ' + value)
+    },
+    error => {console.log(error)},
+    () => { console.log('Cette donnée a été calculée à partir de 4 chiffres')})
+
+console.log('\n');
 
 
